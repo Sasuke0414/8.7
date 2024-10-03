34041ef786b38502a89444e50ab8d75483baffb7
Abdulloh <163301172+Sasuke0414@users.noreply.github.com>
2024-10-03 18:22:27 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,40 @@
+# counties = {
+#     "uzb": 37,
+#     "usa": 350,
+#     "rus": 150
+# } 
+
+# total = 0
+
+# for contry in counties:
+#     print(f"{contry} => {counties[contry]}")
+#     total = total + counties[contry]
+
+# print(total)
+
+# menu = {
+#     "osh": 32,
+#     "manti": 60,
+#     "mastava": 95
+# }
+
+# zakaz = input("Ovqat kiriting: ")
+
+# if zakaz == "osh":
+#     print(menu["osh"])
+# elif zakaz == "mastava":
+#     print(menu["mastava"])
+# elif zakaz == "manti":
+#     print(menu["manti"])
+# else:
+#     print("Bunday ovqat qolmagan !")
+
+text = {
+    "tosh": 3,
+    "sam": 7
+}
+for i in range(3):
+    print(text)
+
+
+
